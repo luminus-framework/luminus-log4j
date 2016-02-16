7626823551f225f56084a26d05926801876a5095
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-02-16 16:56:59 -0500

    bumped up to latest slfj4-log4j

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject luminus-log4j "0.1.2"
+(defproject luminus-log4j "0.1.3"
   :description "Log4j adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-log4j"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.slf4j/slf4j-log4j12 "1.7.14"]
+                 [org.slf4j/slf4j-log4j12 "1.7.16"]
                  [org.apache.logging.log4j/log4j-core "2.5"]])
