600b15f8d31cfdefda6c493523b1438335c6914c
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-07-06 20:57:18 -0400

    added edn config support

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject luminus-log4j "0.1.5"
+(defproject luminus-log4j "0.1.6"
   :description "Log4j adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-log4j"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
+                 [log4j-edn-config "0.1.0"]
                  [org.apache.logging.log4j/log4j-slf4j-impl "2.6.1"]
                  [org.apache.logging.log4j/log4j-core "2.6.1"]])
