27be3d6545ef34c35616b38551fc32154d73813c
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-07-07 22:57:18 -0400

    bumped up log4j-edn-config

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [log4j-edn-config "0.1.0"]
+                 [log4j-edn-config "0.1.1"]
                  [org.apache.logging.log4j/log4j-slf4j-impl "2.6.1"]
                  [org.apache.logging.log4j/log4j-core "2.6.1"]])
