d2a109cd204575867145f9592b3d4a94624d045f
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2021-12-20 10:57:20 -0500

    updated log4j dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus-log4j "0.1.6"
+(defproject luminus-log4j "0.1.7"
   :description "Log4j adapter for Luminus"
   :url "https://github.com/luminus-framework/luminus-log4j"
   :license {:name "Eclipse Public License"
@@ -6,5 +6,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [log4j-edn-config "0.1.1"]
-                 [org.apache.logging.log4j/log4j-slf4j-impl "2.6.1"]
-                 [org.apache.logging.log4j/log4j-core "2.6.1"]])
+                 [org.apache.logging.log4j/log4j-slf4j-impl "2.17.0"]
+                 [org.apache.logging.log4j/log4j-core "2.17.0"]])
